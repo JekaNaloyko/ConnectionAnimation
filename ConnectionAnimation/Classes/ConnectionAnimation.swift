df9ad4476eebd0863823ed14540f24d7011d6eb3
Ievgen Naloiko <ievgen.naloiko@searchconcepts.ch>
2016-08-11 17:54:45 +0300

    Fixed the animation.

--- ConnectionAnimation/Classes/ConnectionAnimation.swift
+++ ConnectionAnimation/Classes/ConnectionAnimation.swift
@@ -10,10 +10,24 @@ import UIKit
 
 public class ConnectionAnimation: UIImageView {
 
+    private var timer: NSTimer?
+    private let imageNamesArray = ["lookingforwifi1", "lookingforwifi2", "lookingforwifi3", "havewifi"]
+    private var currentIndex: Int = 0
+    
     override public func layoutSubviews() {
         super.layoutSubviews()
-        
-        self.image = UIImage(named: "guy.png", inBundle: NSBundle(forClass: ConnectionAnimation.self), compatibleWithTraitCollection: nil)
+        if timer == nil {
+            timer = NSTimer.scheduledTimerWithTimeInterval(0.4, target: self, selector: #selector(ConnectionAnimation.update), userInfo: nil, repeats: true)
+        }
     }
-
+    
+    func update() {
+        self.image =  UIImage(named: imageNamesArray[currentIndex], inBundle: NSBundle(forClass: ConnectionAnimation.self), compatibleWithTraitCollection: nil)
+        if currentIndex >= imageNamesArray.count - 1 {
+            currentIndex = 0
+        }
+        else {
+            currentIndex = currentIndex + 1
+        }
+    }
 }
